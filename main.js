8add18a9a66b8627de9a69a04e8f7f1a00b186e5
Eric-Miles18 <ericmiles199@gmail.com>
2026-06-01 00:28:39 +0300

    Add anitmation to skills progress bars

--- main.js
+++ main.js
@@ -11,3 +11,17 @@ closeMenuBtn.addEventListener("click", (e) => {
   e.stopPropagation();
   menu.classList.remove("active");
 });
+
+// Animate width of skills section
+const skillsContainer = document.getElementById("skills");
+window.onscroll = () => {
+  if (window.scrollY >= skillsContainer.offsetTop - 500) {
+    const skills = document.querySelectorAll(
+      "#skills .box .skill-progress span",
+    );
+
+    skills.forEach((skill) => {
+      skill.style.width = skill.dataset.width;
+    });
+  }
+};
